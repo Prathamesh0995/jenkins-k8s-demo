139ce25f28e5a9801f40c5e85e7d310dd7138caa
Prathamesh Warekar <warekarprathamesh@gmail.com>
2025-06-07 11:25:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins CI/CD on Kubernetes! <br> hello everyone welcome to my portfolio"
+    return "Hello from Jenkins CI/CD on Kubern!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
